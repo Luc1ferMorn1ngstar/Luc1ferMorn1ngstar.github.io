ec3d688b04fce53d9990fca1397495b108541967
Luc1ferMorn1ngstar <Luc1ferMorn1ngstar@users.noreply.github.com>
2018-06-08 20:27:35 +0300

    а
    р

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var $btnTop = $(".top");
+ $(window).on("scroll", function () {
+     if ($(window).scrollTop() > 20) {
+        $btnTop.fadeIn();
+     } else {
+         $btnTop.fadeOut();
+     }
+ })
+$btnTop.on("click", function () {
+   $("html,body").animate({scrollTop:0}, 1500)
+})
